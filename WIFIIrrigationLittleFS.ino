7959171a23bc199ae231f17789b0655f3312b6cf
numerik11 <72150418+numerik11@users.noreply.github.com>
2023-11-21 22:02:12 +1030

    Update WIFIIrrigationLittleFS.ino

--- WIFIIrrigationLittleFS.ino
+++ WIFIIrrigationLittleFS.ino
@@ -20,13 +20,11 @@ const int tankSolenoidPin = D8; // Water tank solenoid
 const int tankLevelPin = A0; // Analog pin for tank level sensor
 
 
-//--------ENTER YOUR DETAILS------------//
+//--------ENTER YOUR DETAILS------------////----Timezone settings in void setup()---///
 const char *ssid = "yrwifissid"; //default wifi creds can be added in AP mode
 const char *password = "yrpassword"; //default wifi creds can be added in AP mode
 String city = "Eden hills, AU"; // Suburb, Country.
 String apiKey = "yrapikeyfrom-openweathermap.org";  //Get a free api at www.openweathermap.org
-//----Timezone settings in void setup()---///
-
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 int addr = 0;
