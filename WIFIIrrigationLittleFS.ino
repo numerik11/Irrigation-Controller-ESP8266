6ac8772e467ab8525854caeaaa8fec02baae5e55
numerik11 <72150418+numerik11@users.noreply.github.com>
2023-11-21 22:02:44 +1030

    Update WIFIIrrigationLittleFS.ino

--- WIFIIrrigationLittleFS.ino
+++ WIFIIrrigationLittleFS.ino
@@ -19,7 +19,6 @@ const int mainsSolenoidPin = D7; // MainsSolenoidPin solenoid
 const int tankSolenoidPin = D8; // Water tank solenoid
 const int tankLevelPin = A0; // Analog pin for tank level sensor
 
-
 //--------ENTER YOUR DETAILS------------////----Timezone settings in void setup()---///
 const char *ssid = "yrwifissid"; //default wifi creds can be added in AP mode
 const char *password = "yrpassword"; //default wifi creds can be added in AP mode
